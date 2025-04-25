1878491a8cf44fb7bb63ddecf74d3183463b24e2
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-25 08:28:38 -0300

    updating .py file

--- manipulando_strings_pandas.py
+++ manipulando_strings_pandas.py
@@ -10,9 +10,19 @@ df = pd.read_csv("dataset.csv")
 
 #print(df["ID_Pedido"].str.split("-").str[1].head())
 
-df["Ano"] = df["ID_Pedido"].str.split("-").str[1]       
-print(df.head())
+# df["Ano"] = df["ID_Pedido"].str.split("-").str[1]       
+# print(df.head())
 
+# print(df.head(3))
+# print(df["Data_Pedido"].head(3))
+# print(df["Data_Pedido"].str.lstrip("20"))
+
+# df["ID_Cliente"] = df["ID_Cliente"].str.replace("CG", "AX")
+# print(df.head())
+
+#print(df.head())
+df["Pedido_Segmento"] = df["ID_Pedido"].str.cat(df["Segmento"], sep = "-")
+print(df.head())
 
 
 
